b6212d51de04d9d94b87b05a8e015a2dfde0d755
tlb-lemrabott <lemrabott.toulba@yahoo.com>
2025-10-28 22:41:04 -0400

    Update backend-api/server.js

--- backend-api/server.js
+++ backend-api/server.js
@@ -1,7 +1,7 @@
 import { createServer } from 'http';
 import app from './src/app.js';
 
-const PORT = process.env.PORT || 3400;
+const PORT = process.env.PORT || 3000;
 
 const server = createServer(app);
 
